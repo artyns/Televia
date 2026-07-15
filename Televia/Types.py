beb44c2891666717abd6ed145d2b082c808d48fd
artyns <109096299+artyns@users.noreply.github.com>
2026-07-15 21:45:52 +0330

    add receiver_user_id, sendSticker support

--- Televia/Types.py
+++ Televia/Types.py
@@ -8,6 +8,9 @@ class Message:
         self.message_id = data.get("message_id")
         self.text = data.get("text")
         self.caption = data.get("caption")
+        self.receiver_user = data.get("receiver_user")
+        self.ephemeral_message_id = data.get("ephemeral_message_id")
+        self.date = data.get("date")
 
         photo_data = data.get("photo")
         self.photo = photo(photo_data[-1]) if photo_data else None
@@ -16,6 +19,7 @@ class Message:
         self.from_user = User(data.get("from", {}))
         self.forward_from = User(data["forward_from"]) if data.get("forward_from") else None
         self.reply_to_message = Message(data["reply_to_message"], bot=bot) if data.get("reply_to_message") else None
+        self.quote = Quote(data.get("TextQuote"))
 
         self.guest_query_id = data["guest_query_id"] if data.get("guest_query_id") else None
 
@@ -32,7 +36,7 @@ class User:
     def __init__(self, data):
         self.id = data.get("id")
         self.lang_code = data.get("language_code")
-        self.user_name = data.get("username")
+        self.username = data.get("username")
         self.first_name = data.get("first_name")
         self.last_name = data.get("last_name")
 
@@ -42,6 +46,13 @@ class User:
 class photo:
     def __init__(self, data):
         self.file_id = data.get("file_id")
+
+
+class Quote:
+    def __init__(self, data):
+        self.text = data.get("text")
+        self.position = data.get("position")
+        self.is_manual = data.get("is_manual")
 
 
 #inline keyboard button
--- Televia/bot.py
+++ Televia/bot.py
@@ -50,7 +50,6 @@ class Bot:
                 "error": str(e)
             }
 
-
     def message_handler(self, commands=None, func=None, content_types=None):
         """
         message handler
@@ -114,7 +113,7 @@ class Bot:
 
 
     # send message
-    def send_message(self, chat_id, text, parse_mode=None,reply_markup=None, protect_content=False, message_effect_id=None):
+    def send_message(self, chat_id, text, receiver_user_id=None, parse_mode=None,reply_markup=None, protect_content=False, message_effect_id=None):
         """
         send a message to chat id
         """
@@ -123,7 +122,8 @@ class Bot:
             "text": text,
             "parse_mode": parse_mode if parse_mode else self.parse_mode,
             "protect_content": protect_content,
-            "message_effect_id": message_effect_id
+            "message_effect_id": message_effect_id,
+            "receiver_user_id":receiver_user_id,
             }
         
         if reply_markup is not None:
@@ -237,7 +237,7 @@ class Bot:
         return self.request("setMessageReaction", data=data)
     
     
-    def send_photo(self, chat_id, photo, caption=None, reply_to_message_id=None, parse_mode=None, has_spoiler=False, protect_content=False, message_effect_id=None, reply_markup=None):
+    def send_photo(self, chat_id, photo, caption=None, receiver_user_id=None, reply_to_message_id=None, parse_mode=None, has_spoiler=False, protect_content=False, message_effect_id=None, reply_markup=None):
         """
         send photo with URL or photo file
         """
@@ -266,7 +266,7 @@ class Bot:
             return Message(res["result"], bot=self)
         return None
 
-    def send_video(self, chat_id, video, caption=None, reply_to_message_id=None, parse_mode=None, has_spoiler=False, protect_content=False, message_effect_id=None, reply_markup=None):
+    def send_video(self, chat_id, video, caption=None, receiver_user_id=None, reply_to_message_id=None, parse_mode=None, has_spoiler=False, protect_content=False, message_effect_id=None, reply_markup=None):
         """
         send Video with URL or photo file
         """
@@ -295,7 +295,7 @@ class Bot:
             return Message(res["result"], bot=self)
         return None
 
-    def send_audio(self, chat_id, audio, caption=None, reply_to_message_id=None, parse_mode=None, has_spoiler=False, protect_content=False, message_effect_id=None, reply_markup=None):
+    def send_audio(self, chat_id, audio, caption=None, receiver_user_id=None, reply_to_message_id=None, parse_mode=None, has_spoiler=False, protect_content=False, message_effect_id=None, reply_markup=None):
         """
         send audio with URL or photo file
         """
@@ -336,6 +336,32 @@ class Bot:
             return Message(res["result"], bot=self)
         return None
 
+    def send_sticker(self, chat_id, sticker, receiver_user_id=None, emoji=None, protect_content=None, message_effect_id=None, reply_markup=None):
+        data = {
+            "chat_id": chat_id,
+            "receiver_user_id": receiver_user_id,
+            "emoji": emoji,
+            "protect_content": protect_content,
+            "message_effect_id": message_effect_id
+        }
+
+        if reply_markup is not None:
+            if hasattr(reply_markup, "to_dict"):
+                data["reply_markup"] = json.dumps(reply_markup.to_dict())
+
+        if isinstance(sticker, str):
+            data["sticker"] = sticker
+            res = self.request("sendSticker", data=data)
+        else:
+            files = {"sticker": sticker}
+            res = self.request("sendSticker", data=data, files=files)
+
+        if res.get("ok"):
+            return Message(res["result"], bot=self)
+        return None
+
+        
+
     def send_message_draft(self, chat_id, draft_id, text=None, parse_mode=None):
         data = {
             "chat_id": chat_id,
@@ -358,7 +384,7 @@ class Bot:
         else:
             return res
 
-    def reply_to(self, message, text, message_id=None, parse_mode=None,protect_content=False, message_effect_id=None, reply_markup=None):
+    def reply_to(self, message, text, message_id=None,receiver_user_id=None, parse_mode=None,protect_content=False, message_effect_id=None, reply_markup=None):
         """
         reply to message \n
         reply to selected message with message_id
@@ -369,6 +395,7 @@ class Bot:
                 "parse_mode": parse_mode if parse_mode else self.parse_mode,
                 "protect_content": protect_content,
                 "message_effect_id": message_effect_id,
+                "receiver_user_id": receiver_user_id
                 
                 }
         
